0e9d6653e70858381e5909e5a1512e99d6b2d5b4
Arpit Gogia <aarpitgogia@gmail.com>
2018-08-22 20:33:07 +0530

    Removed constants from their separate file

--- rake.go
+++ rake.go
@@ -8,6 +8,21 @@ import (
 	"strings"
 )
 
+// ForWordSplit - Splitting Words
+const ForWordSplit = "[\\p{L}\\d_]+"
+
+// ForStopWordDetection - Filtering stop words
+const ForStopWordDetection = `(?:\A|\z|\s)%s(?:\A|\z|\s)`
+
+// ForSplittingSentences - Splitting Sentences
+const ForSplittingSentences = `[.,\/#!$%\^&\*;:{}=\-_~()]`
+
+// TextFilename - The file from which text is read for analysis
+const TextFilename = "text.txt"
+
+// StopwordFilename - The file of which each line is a stop word
+const StopwordFilename = "SmartStoplist.txt"
+
 type rakeScore struct {
 	word  string
 	score float64
