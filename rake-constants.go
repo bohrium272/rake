ea33aba556609e169684771b5628237dc1ed5cc2
Arpit Gogia <aarpitgogia@gmail.com>
2018-08-27 23:35:33 +0530

    Remove stopwords file

--- rake-constants.go
+++ rake-constants.go
@@ -3,9 +3,6 @@ package rake
 // textFilename - The file from which text is read for analysis
 const textFilename = "text.txt"
 
-// stopwordFilename - The file of which each line is a stop word
-const stopwordFilename = "SmartStoplist.txt"
-
 // sampleText - Sample Text
 const sampleText = "The growing doubt of human autonomy and reason has created a state of moral confusion where man is left without the guidance of either revelation or reason. The result is theacceptance of a relativistic position which proposes that value judgements and ethical norms are exclusively matters of arbitrary preference and that no objectively valid statementcan be made in this realm... But since man cannot live without values and norms, this relativism makes him an easy prey for irrational value systems."
 
--- rake.go
+++ rake.go
@@ -1,7 +1,6 @@
 package rake
 
 import (
-	"fmt"
 	"io/ioutil"
 	"sort"
 	"strings"
@@ -137,7 +136,6 @@ func sortScores(scores map[string]float64, topN int) []Score {
 		rakeScores = append(rakeScores, Score{k, v})
 	}
 	sort.Sort(byScore(rakeScores))
-	fmt.Println(rakeScores)
 	if topN < len(rakeScores) && topN > 0 {
 		return rakeScores[0:topN]
 	}
